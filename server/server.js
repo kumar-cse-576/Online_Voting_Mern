4d727eb33870569b0150c0e93dbbdcf001c93a3a
Bhemarasetti Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2025-11-09 15:48:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,20 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/admin', require('./routes/adminRoutes'));
-// THIS must be present
-// ✅ Register auth route
-
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/vote', require('./routes/voteRoutes_backup'));
 app.use('/api/vote', require('./routes/voteRoutes'));
 
 
 
-// ✅ MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/votingdb")
-  .then(() => console.log('✅ Connected to MongoDB locally'))
-  .catch((err) => console.error('❌ MongoDB connection error:', err));
+  .then(() => console.log(' Connected to MongoDB locally'))
+  .catch((err) => console.error(' MongoDB connection error:', err));
 
-// ✅ Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
